9295d326672cd794ef908c0008f1b95e963767a0
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-03 15:05:51 +0300

    Use bluebird.try to catch both type of rejections

--- src/bin/setVersion.ts
+++ src/bin/setVersion.ts
@@ -4,6 +4,7 @@ import { writeJsonFile } from '../helpers/writeJsonFile';
 import path from 'path';
 import { trim } from 'lodash';
 import { stripIndent } from 'common-tags';
+import bluebird from 'bluebird';
 
 /**
  * This script is used during the deployment of packages that need to have a
@@ -40,10 +41,13 @@ async function setVersion() {
   if (!version) {
     // tslint:disable-next-line:no-multiline-string
     throw new TypeError(stripIndent`
-    Expected a semver version to be specified for set-version.
-    Make sure a version is passed like this:
-    yarn set-version 1.4.5
-    or you have an environment variable named "$TRAVIS_TAG"
+      Expected a semver version to be specified for set-version.
+      
+      Make sure a version is passed like this:
+
+        yarn set-version 1.4.5
+      
+      or that you have an environment variable named "$TRAVIS_TAG"
     `);
   }
 
@@ -52,7 +56,11 @@ async function setVersion() {
   await writeJsonFile(packageJsonPath, { ...packageJson, version });
 }
 
-setVersion().catch(error => {
-  console.error('Could not set version in package.json:', error.message);
-  process.exit(1);
-});
+bluebird
+  .try(async () => {
+    await setVersion();
+  })
+  .catch(error => {
+    console.error('Could not set version in package.json:', error.message);
+    process.exit(1);
+  });
