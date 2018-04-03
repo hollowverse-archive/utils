b3981babec4d6dcd7415e00525d0bf66b3ffbf5d
wholesomedev <mk@hollowverse.com>
2018-04-02 18:56:05 -0700

    Allow `v` in TRAVIS_TAG (#8)

--- helpers/setVersion.ts
+++ helpers/setVersion.ts
@@ -11,11 +11,24 @@ import { trim } from 'lodash';
  * The script sets `version` key in `package.json` during deployment to the
  * value of `TRAVIS_TAG`. That way we don't have to remember to set the version
  * key in `package.json` before publishing.
+ *
+ * If `TRAVIS_TAG` starts with a `v` such as `v4.0.2`, this script will strip out the `v`
+ * to make `TRAVIS_TAG` semver-compatible.
  */
+function getVersion() {
+  const version = trim(process.env.TRAVIS_TAG);
+
+  if (version[0] === 'v') {
+    return version.substr(1);
+  }
+
+  return version;
+}
+
 async function setVersion() {
   const packagejsonPath = path.resolve(process.cwd(), 'package.json');
   const packageJson = require(packagejsonPath); // tslint:disable-line no-require-imports non-literal-require
-  const version = trim(process.env.TRAVIS_TAG);
+  const version = getVersion();
 
   if (version) {
     await writeJsonFile(packagejsonPath, { ...packageJson, version });
